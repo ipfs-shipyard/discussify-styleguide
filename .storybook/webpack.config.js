c1c15b546c81f9aa1133fbb1125d82d7cab56562
André Cruz <andremiguelcruz@msn.com>
2018-07-19 14:05:25 +0100

    chore: simplify configs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const SvgStorePlugin = require('external-svg-sprite-loader/lib/SvgStorePlugin');
 
-module.exports = (storybookBaseConfig, configType) => {
+module.exports = (config) => {
     // CSS files loader for node_modules
-    storybookBaseConfig.module.rules.push({
+    config.module.rules.push({
         test: /\.css$/,
         include: path.join(__dirname, '../node_modules'),
         loader: [
@@ -12,18 +12,12 @@ module.exports = (storybookBaseConfig, configType) => {
         ],
     });
 
-    // CSS files loader which enables the use of postcss
-    storybookBaseConfig.module.rules.push({
+    // Enable CSS modules and PostCSS
+    config.module.rules.push({
         test: /\.css$/,
         exclude: path.join(__dirname, '../node_modules'),
         loader: [
-            {
-                // Extract CSS files if we are not in development mode
-                loader: 'style-loader',
-                options: {
-                    convertToAbsoluteUrls: true,
-                },
-            },
+            'style-loader',
             {
                 loader: 'css-loader',
                 options: {
@@ -49,14 +43,13 @@ module.exports = (storybookBaseConfig, configType) => {
     // Load SVG files and create an external sprite
     // While this has a lot of advantages such as not blocking the initial load,
     // it might not workout for every SVG, see: https://github.com/moxystudio/react-with-moxy/issues/6
-    storybookBaseConfig.module.rules.push({
+    config.module.rules.push({
         test: /\.svg$/,
-        exclude: [/\.inline\.svg$/, path.join(__dirname, '../src/media/fonts')],
         use: [
             {
                 loader: 'external-svg-sprite-loader',
                 options: {
-                    name: 'images/svg-sprite.svg',
+                    name: 'static/media/svg-sprite.svg',
                 },
             },
             // Uniquify classnames and ids so that if svgxuse injects the sprite into the body,
@@ -69,45 +62,20 @@ module.exports = (storybookBaseConfig, configType) => {
             },
         ],
     });
-    storybookBaseConfig.plugins.push(new SvgStorePlugin());
-
-    // Support inline svgs, see explanation above
-    storybookBaseConfig.module.rules.push({
-        test: /\.inline\.svg$/,
-        use: [
-            'raw-loader',
-            {
-                loader: 'svgo-loader',
-                options: {
-                    plugins: [
-                        { removeTitle: true },
-                        { removeDimensions: true },
-                        { cleanupIDs: false },
-                    ],
-                },
-            },
-            // Uniquify classnames and ids so they don't conflict with eachother
-            {
-                loader: 'svg-css-modules-loader',
-                options: {
-                    transformId: true,
-                },
-            },
-        ],
-    });
+    config.plugins.push(new SvgStorePlugin());
 
     // Support web fonts
-    storybookBaseConfig.module.rules.push({
+    config.module.rules.push({
         test: /\.(woff2|woff)$/,
         use: [
             {
                 loader: 'file-loader',
                 options: {
-                    name: 'fonts/[name].[ext]',
+                    name: 'static/media/[name].[ext]',
                 },
             },
         ],
     });
 
-    return storybookBaseConfig;
+    return config;
 };
